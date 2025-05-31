16bd8a3a59fd80b23ec98c5a5df1ba04859c5ac1
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-05-31 22:54:58 +0700

    chore: added logging

--- produce/kafka.py
+++ produce/kafka.py
@@ -101,6 +101,8 @@ from datetime import datetime
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from config.logging import Logger
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger(__name__)
 
 class Producer:
     def __init__(self, kafka_broker: str, kafka_topic: str) -> None:
